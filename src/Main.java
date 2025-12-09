4eccec247e05713a435e210d20bb9a3a6191e21e
Alternade <aptsahu2@gmail.com>
2025-12-09 18:08:59 +0530

    Added : UC1/ Ability to create contact

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Contact c1 = new Contact("hey","Hello","somewhere","nowhere","maybe","Dont Know","dornt reach me",1111111);
+        System.out.println(c1);
+    }
+}
